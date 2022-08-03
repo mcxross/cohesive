626c933eafc881f8b6536fe4307d731eefa0c5da
@a <28899947+astinz@users.noreply.github.com>
2022-08-03 15:24:49 +0300

    improve file tree...

---
+++ common/src/androidMain/kotlin/com/mcxross/cohesive/common/editor/platform/Mouse.kt
@@ -0,0 +1,12 @@
+package com.mcxross.cohesive.common.editor.platform
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+
+actual fun Modifier.pointerMoveFilter(
+    onEnter: () -> Boolean,
+    onExit: () -> Boolean,
+    onMove: (Offset) -> Boolean
+): Modifier = this
+
+actual fun Modifier.cursorForHorizontalResize() = this
