ce3961f6f8b341ae9ac3736f68c441660efd8aeb
@a <28899947+astinz@users.noreply.github.com>
2023-07-18 11:17:17 +0300

    [cmpe] native distro update, incomplete...

--- cmpe/desktop/build.gradle.kts
+++ cmpe/desktop/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
   kotlin("multiplatform")
 
   id("org.jetbrains.compose")
-  id("org.jetbrains.dokka")
 }
 
 group = "com.mcxross.cohesive"
@@ -36,10 +35,21 @@ compose.desktop {
       targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
       packageName = "cohesive"
       packageVersion = "1.0.0"
+      version = "0.1.0-alpha"
+      description = "Blockchain Agnostic Development Platform"
+      vendor = "Mcxross"
+      licenseFile.set(rootProject.file("LICENSE"))
+
+      macOS {}
+      windows {
+        console = true
+        dirChooser = true
+        perUserInstall = true
+        menuGroup = "Mcxross"
+        upgradeUuid = "70bdd2db-856c-4770-b936-1dca129752c2"
+      }
+      linux {}
+
     }
   }
 }
-
-/*tasks.named("run") {
-  //systemProperty("cps.mode", "development")
-}*/
